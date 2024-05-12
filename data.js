8a076f5a992ada2b33ca5b0a0e3eda2868027531
iamromand <romand@gmail.com>
2024-05-12 19:50:06 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,344 @@
+var itemsFromData = {
+    "items": [
+        {
+            "key": "seasons",
+            "title": "Seasons",
+            "options": [
+                {
+                    "key": "summer",
+                    "name": "Summer",
+                    "advantages": "Soon",
+                    "disadvantages": "Hot"
+                },
+                {
+                    "key": "fall",
+                    "name": "Fall",
+                    "advantages": "Colder weather",
+                    "disadvantages": "Longer wait"
+                }
+            ]
+        },
+        {
+            "key": "months",
+            "title": "Months",
+            "options": [
+                {
+                    "key": "june1",
+                    "name": "June - first Half",
+                    "info": "Soon"
+                },
+                {
+                    "key": "june2",
+                    "name": "June - second Half",
+                    "disadvantages": "Football championship, children holidays"
+                },
+                {
+                    "key": "july1",
+                    "name": "July - first Half",
+                    "disadvantages": "Football championship, children holidays"
+                },
+                {
+                    "key": "july2",
+                    "name": "July - second Half",
+                    "disadvantages": "children holidays"
+                },
+                {
+                    "key": "august1",
+                    "name": "August - first Half",
+                    "disadvantages": "children holidays"
+                },
+                {
+                    "key": "august2",
+                    "name": "August - second Half",
+                    "disadvantages": "children holidays"
+                },
+                {
+                    "key": "september1",
+                    "name": "September - first Half",
+                    "advantages": "cheaper",
+                    "disadvantages": "near Greece vacation for Melanie"
+                },
+                {
+                    "key": "september2",
+                    "name": "September - second Half",
+                    "advantages": "cheaper",
+                    "disadvantages": "near Greece vacation for Melanie"
+                },
+                {
+                    "key": "october1",
+                    "name": "October - first Half",
+                    "advantages": "cheaper",
+                    "disadvantages": "longer wait, new semester?"
+                },
+                {
+                    "key": "october2",
+                    "name": "October - second Half",
+                    "advantages": "cheaper",
+                    "disadvantages": "longer wait"
+                },
+            ]
+        },
+        {
+            "key": "location",
+            "title": "Location",
+            "options": [
+                {
+                    "key": "bigcity",
+                    "name": "Big City",
+                    "advantages": "Restaraunts & cafes,Museums,can split to separate groups",
+                    "disadvantages": "More expensive"
+                },
+                {
+                    "key": "smallcity",
+                    "name": "Small City",
+                    "advantages": "",
+                    "disadvantages": ""
+                },
+                {
+                    "key": "forest",
+                    "name": "Forest",
+                    "advantages": "Nice walks,Nature,Cheaper",
+                    "disadvantages": "Harder to reach with public transportation (Both Danielle and I can rent a car)"
+                },
+                {
+                    "key": "lake",
+                    "name": "Lake",
+                    "advantages": "Nice walks,Nature,Cheaper",
+                    "disadvantages": "Harder to reach with public transportation (Both Danielle and I can rent a car)"
+                }
+            ]
+        },
+        {
+            "key": "transportation",
+            "title": "Transportation",
+            "options": [
+                {
+                    "key": "airplane",
+                    "name": "Airplane",
+                    "advantages": "",
+                    "disadvantages": ""
+                },
+                {
+                    "key": "train",
+                    "name": "Train",
+                    "advantages": "",
+                    "disadvantages": ""
+                },
+                {
+                    "key": "car",
+                    "name": "Car",
+                    "advantages": "",
+                    "disadvantages": ""
+                }
+            ]
+        },
+        {
+            "key": "distance",
+            "title": "Distance",
+            "options": [
+                {
+                    "key": "50",
+                    "name": "Up to about 50km",
+                    "advantages": "",
+                    "disadvantages": ""
+                },
+                {
+                    "key": "200",
+                    "name": "Up to about 200km",
+                    "advantages": "",
+                    "disadvantages": ""
+                },
+                {
+                    "key": "germany",
+                    "name": "Within Germany",
+                    "advantages": "",
+                    "disadvantages": ""
+                },
+                {
+                    "key": "europe",
+                    "name": "Within Europe",
+                    "advantages": "",
+                    "disadvantages": ""
+                }
+            ]
+        },
+        {
+            "key": "attractions",
+            "title": "Attractions",
+            "options": [
+                {
+                    "key": "zoo",
+                    "name": "Zoo",
+                    "advantages": "",
+                    "disadvantages": ""
+                },
+                {
+                    "key": "museum",
+                    "name": "Museums",
+                    "advantages": "",
+                    "disadvantages": ""
+                }
+            ]
+        },
+        {
+            "key": "time",
+            "title": "Time",
+            "options": [
+                {
+                    "key": "day",
+                    "name": "Day trip",
+                    "advantages": "",
+                    "disadvantages": ""
+                },
+                {
+                    "key": "night",
+                    "name": "Over night",
+                    "advantages": "",
+                    "disadvantages": ""
+                },
+                {
+                    "key": "weekend",
+                    "name": "Weekend",
+                    "advantages": "",
+                    "disadvantages": ""
+                },
+                {
+                    "key": "2",
+                    "name": "Two nights",
+                    "advantages": "",
+                    "disadvantages": ""
+                },
+                {
+                    "key": "3",
+                    "name": "Three nights",
+                    "advantages": "",
+                    "disadvantages": ""
+                },
+                {
+                    "key": "4",
+                    "name": "Four nights",
+                    "advantages": "",
+                    "disadvantages": ""
+                },
+                {
+                    "key": "plus",
+                    "name": "5+ nights",
+                    "advantages": "",
+                    "disadvantages": ""
+                }
+            ]
+        },
+        {
+            "key": "accomodations",
+            "title": "Accomodations",
+            "options": [
+                {
+                    "key": "hotel",
+                    "name": "Hotel",
+                    "advantages": "",
+                    "disadvantages": "More expensive"
+                },
+                {
+                    "key": "aparthotel",
+                    "name": "Apart-hotel",
+                    "advantages": "",
+                    "disadvantages": ""
+                },
+                {
+                    "key": "airbnb",
+                    "name": "Air-bnb",
+                    "advantages": "",
+                    "disadvantages": ""
+                },
+                {
+                    "key": "tent",
+                    "name": "Tent",
+                    "advantages": "",
+                    "disadvantages": ""
+                },
+                {
+                    "key": "cabin",
+                    "name": "Cabin in the woods",
+                    "advantages": "",
+                    "disadvantages": ""
+                }
+            ]
+        },
+        {
+            "key": "Cities",
+            "title": "Cities",
+            "options": [
+                {
+                    "key": "kassel",
+                    "name": "Kassel",
+                    "advantages": "",
+                    "disadvantages": ""
+                },                {
+                    "key": "munich",
+                    "name": "Munich",
+                    "advantages": "",
+                    "disadvantages": ""
+                },                {
+                    "key": "heidelberg",
+                    "name": "Heidelberg",
+                    "advantages": "",
+                    "disadvantages": ""
+                },                {
+                    "key": "stuttgart",
+                    "name": "Stuttgart",
+                    "advantages": "",
+                    "disadvantages": ""
+                },                {
+                    "key": "wurzburg",
+                    "name": "Würzburg",
+                    "advantages": "",
+                    "disadvantages": ""
+                },
+                {
+                    "key": "nuremberg",
+                    "name": "Nuremberg",
+                    "advantages": "",
+                    "disadvantages": ""
+                },
+                {
+                    "key": "berlin",
+                    "name": "Berlin",
+                    "advantages": "",
+                    "disadvantages": ""
+                },                {
+                    "key": "cologne",
+                    "name": "Cologne",
+                    "advantages": "",
+                    "disadvantages": ""
+                },
+                {
+                    "key": "hamburg",
+                    "name": "Hamburg",
+                    "advantages": "",
+                    "disadvantages": ""
+                },
+                {
+                    "key": "austria",
+                    "name": "Austria",
+                    "info": "4-6 hours"
+                },
+                {
+                    "key": "italy",
+                    "name": "Italy",
+                    "info": "bad train connections"
+                },                {
+                    "key": "amsterdam",
+                    "name": "Amsterdam",
+                    "info": ""
+                },
+                {
+                    "key": "france",
+                    "name": "France",
+                    "advantages": "",
+                    "disadvantages": ""
+                }
+            ]
+        }
+    ],
+    "interactions": []
+}
